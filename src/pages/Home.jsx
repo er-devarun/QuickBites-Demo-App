34c9bf08b50791c38e1eeac80a936134d268dc75
er-devarun <137439924+er-devarun@users.noreply.github.com>
2024-02-02 19:12:39 +0530

    Update Home.jsx
    Error fixed, the component "Navbar.jsx" imported in the Home.jsx but it was imported as "NavBar.jsx" case problem.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import CartBox from "../components/CartBox";
 import CategoryMenu from "../components/CategoryMenu";
 import FoodItemContainer from "../components/FoodItemContainer";
-import Navbar from "../components/NavBar";
+import Navbar from "../components/Navbar";
 import foodData from "../data/foodData";
 import {useDispatch} from 'react-redux';
 import { homeItemActions } from "../redux_store/homeItemSlice";
